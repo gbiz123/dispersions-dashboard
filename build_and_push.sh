e9c9d1698d3c96acd88b1bf8df771f2641cc59f4
greg bizup <g.bizup@gmail.com>
2025-07-28 16:02:31 -0400

    stuff

--- build_and_push.sh
+++ build_and_push.sh
@@ -4,4 +4,5 @@ image_name=${PWD##*/}
 echo "$image_name"
 docker build -t "$image_name" .
 docker tag "$image_name" "registry.digitalocean.com/dispersions/$image_name"
-docker push "registry.digitalocean.com/dispersions/$gateway"
+docker push "registry.digitalocean.com/dispersions/$image_name"
+kubectl rollout restart deployment/$image_name
